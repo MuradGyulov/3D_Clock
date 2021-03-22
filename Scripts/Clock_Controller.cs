30f7ccf440de992df36ee3ff9ae9c5b9a1e25f04
MuradGyulov <80948753+MuradGyulov@users.noreply.github.com>
2021-03-22 08:38:28 +0300

    1

---
+++ Scripts/Clock_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock_Controller : MonoBehaviour
+{
+    [SerializeField] Transform Hour_Hand, Minute_Hand, Second_Hand;
+
+    private const float Hourse_To_Degrees = 30f;
+    private const float Minute_To_Degrees = 6f;
+    private const float Secont_To_Degrees = 6f;
+
+    private void Update()
+    {
+        var time = DateTime.Now;
+
+        Hour_Hand.localRotation = Quaternion.Euler(0, Hourse_To_Degrees * time.Hour, 0);
+        Minute_Hand.localRotation = Quaternion.Euler(0, Minute_To_Degrees * time.Minute, 0);
+        Second_Hand.localRotation = Quaternion.Euler(0, Secont_To_Degrees * time.Second, 0);
+    }
+}
